11407ac5902d58b05e12a79a94c5c20740f12add
Adam Nowak <anowak2000@gmail.com>
2024-08-08 14:57:32 +0200

    download pdb_seqres when initializing

---
+++ initialize_hpc_helios.sh
@@ -0,0 +1,52 @@
+GROUP_DIR="$PLG_GROUPS_STORAGE/plggsano/tomaszlab"
+
+set -x
+set -e
+
+if [[ ! -d "deepfri" ]]
+then
+  mkdir deepfri
+fi
+
+cd deepfri
+
+DATA_DIR="deepfri/dev_data"
+mkdir "$GROUP_DIR/$DATA_DIR"
+
+module load GCCcore
+module load git
+module load Miniconda3
+eval "$(conda shell.bash hook)"
+
+
+ENV_PATH="$GROUP_DIR/deepfri/dev_env"
+
+if [[ -d "deepFRI2-toolbox-dev" ]]
+then
+  cd "deepFRI2-toolbox-dev"
+  git pull
+
+  conda activate $ENV_PATH
+
+else
+  git clone https://github.com/youngdashu/deepFRI2-toolbox-dev.git
+  cd "deepFRI2-toolbox-dev"
+  echo "DATA_PATH=$GROUP_DIR/$DATA_DIR" > .env
+  echo "SEPARATOR=-" >> .env
+
+  CONDA_DIR="$GROUP_DIR/.conda"
+  mkdir -p "$CONDA_DIR"
+  conda config --add pkgs_dirs "$CONDA_DIR"
+
+  conda env create --prefix $ENV_PATH --file "dev_env_conda.yml"
+
+  conda config --set auto_activate_base false
+
+  conda activate $ENV_PATH
+
+  pip install bio~=1.7.0
+  pip install foldcomp~=0.0.7
+
+fi
+
+PYTHONPATH='.' python3 -u ./toolbox/initialize_repository.py
--- toolbox/initialize_repository.py
+++ toolbox/initialize_repository.py
@@ -1,6 +1,32 @@
+import os
+import gzip
+import requests
+
 def initialize():
-    # download PDB fasta file
+    download_seqres()
     pass
+
+def download_seqres():
+    url = "https://files.rcsb.org/pub/pdb/derived_data/pdb_seqres.txt.gz"
+    local_filename = "pdb_seqres.txt.gz"
+
+    # Download the file
+    response = requests.get(url, stream=True)
+    with open(local_filename, "wb") as file:
+        for chunk in response.iter_content(chunk_size=1024):
+            file.write(chunk)
+
+    print(f"Downloaded {local_filename}")
+
+    # Extract the contents
+    with gzip.open(local_filename, "rb") as gz_file:
+        extracted_content = gz_file.read()
+
+    extracted_filename = os.path.splitext(local_filename)[0]
+    with open(extracted_filename, "wb") as file:
+        file.write(extracted_content)
+
+    print(f"Extracted contents to {extracted_filename}")
 
 
 if __name__ == "__main__":
