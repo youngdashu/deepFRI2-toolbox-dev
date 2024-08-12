73cf50ccc39f2c60f3599e22f3606b900364d53a
Adam Nowak <anowak2000@gmail.com>
2024-08-12 03:34:41 +0200

    changed core calculate and increased worker memory limit

--- toolbox/models/manage_dataset/structures_dataset.py
+++ toolbox/models/manage_dataset/structures_dataset.py
@@ -31,13 +31,14 @@ SEPARATOR = os.getenv("SEPARATOR")
 
 def create_client():
     # Get the total number of CPUs available on the machine
-    total_cores = os.cpu_count()
+    total_cores = os.cpu_count() // 2
 
     # Create a LocalCluster with the calculated number of workers
     cluster = LocalCluster(
         dashboard_address='127.0.0.1:8786',
         n_workers=total_cores,
         threads_per_worker=1,
+        memory_limit='3.5 GiB',
         silence_logs=logging.ERROR
     )
 
