fbf6654afe360e007a89cab24d0faeba12c83b06
Adam Nowak <anowak2000@gmail.com>
2024-08-12 15:16:24 +0200

    cores in slurm handling

--- toolbox/models/utils/create_client.py
+++ toolbox/models/utils/create_client.py
@@ -6,14 +6,16 @@ from distributed import LocalCluster, Client
 
 def create_client():
     # Get the total number of CPUs available on the machine
-    total_cores = os.cpu_count()
+    total_cores = os.environ.get('SLURM_CPUS_PER_TASK')
+    if total_cores is None:
+        total_cores = os.cpu_count()
 
     # Create a LocalCluster with the calculated number of workers
     cluster = LocalCluster(
         dashboard_address='127.0.0.1:8786',
-        n_workers=total_cores,
+        n_workers=int(total_cores) - 2,
         threads_per_worker=1,
-        memory_limit='3.5 GiB',
+        memory_limit='4 GiB',
         silence_logs=logging.ERROR
     )
 
