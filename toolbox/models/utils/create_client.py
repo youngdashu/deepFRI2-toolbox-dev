bd830f26d416c8c60680c0502bb7d05f19825545
Adam Nowak <anowak2000@gmail.com>
2024-08-25 19:41:52 +0200

    added MPI cluster for HPC

--- toolbox/models/utils/create_client.py
+++ toolbox/models/utils/create_client.py
@@ -1,28 +1,36 @@
 import logging
 import os
 
+import dask_mpi
 from dask.distributed import LocalCluster, Client
 
 import warnings
 import distributed
 
 
-def create_client():
+
+def create_client(is_slurm_client: bool):
     # Get the total number of CPUs available on the machine
-    total_cores = os.environ.get('SLURM_CPUS_PER_TASK')
-    if total_cores is None:
-        total_cores = os.cpu_count()
 
-    # Create a LocalCluster with the calculated number of workers
-    cluster = LocalCluster(
-        dashboard_address='0.0.0.0:8989',
-        n_workers=int(total_cores) - 2,
-        threads_per_worker=1,
-        memory_limit='16 GiB',
-        silence_logs=logging.ERROR
-    )
+    if is_slurm_client:
+        dask_mpi.initialize(dashboard_address='0.0.0.0:8989', memory_limit='16 GiB',)
+        client = Client()
+    else:
 
-    client = Client(cluster)
+        total_cores = os.environ.get('SLURM_CPUS_PER_TASK')
+        if total_cores is None:
+            total_cores = os.cpu_count()
+
+        # Create a LocalCluster with the calculated number of workers
+        cluster = LocalCluster(
+            dashboard_address='0.0.0.0:8989',
+            n_workers=int(total_cores) - 2,
+            threads_per_worker=1,
+            memory_limit='16 GiB',
+            silence_logs=logging.ERROR
+        )
+
+        client = Client(cluster)
     print(client.dashboard_link)
 
     warnings.simplefilter("ignore", distributed.comm.core.CommClosedError)
