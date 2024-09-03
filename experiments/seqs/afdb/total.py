fff43a8b3c833fcb06ab931f45082ecab84b9dce
Adam Nowak <anowak2000@gmail.com>
2024-09-03 12:55:05 +0200

    plotting

--- experiments/seqs/afdb/total.py
+++ experiments/seqs/afdb/total.py
@@ -2,7 +2,7 @@ from experiments.plot_from_htmls import plot_from_htmls
 
 
 if __name__ == "__main__":
-    html_files_dir = "/Users/youngdashu/sano/deepFRI2-toolbox-dev/experiments/seqs/reports_seq"  # replace with actual path
+    html_files_dir = "/Users/youngdashu/sano/deepFRI2-toolbox-dev/experiments/seqs/afdb/reports_seq"  # replace with actual path
     task_names = [
         "get_sequences_from_batch",
 
---
+++ experiments/seqs/pdb/total.py
@@ -0,0 +1,9 @@
+from experiments.plot_from_htmls import plot_from_htmls
+
+
+if __name__ == "__main__":
+    html_files_dir = "/Users/youngdashu/sano/deepFRI2-toolbox-dev/experiments/seqs/pdb/reports"  # replace with actual path
+    task_names = [
+        "get_sequences_from_batch",
+    ]
+    plot_from_htmls(html_files_dir=html_files_dir, task_names=task_names)
